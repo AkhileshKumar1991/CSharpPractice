798c877bd9e605830649d8c96d42561cd8aff5b9
Akhilesh Kumar <kumar11akhilesh@gmail.com>
2022-04-01 12:24:52 +0530

    Akhiesh

---
+++ CSharpPractice/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Person
+    {
+        public string name { get; set; }
+
+        public override string ToString()
+        {
+            return "My name is " +name;
+        }
+    }
+}
--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -10,6 +10,22 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
+            int cout = 3;
+
+            Person[] person = new Person[cout];
+
+            for(int i=0; i<3; i++)
+            {
+                person[i] = new Person() { name = Console.ReadLine() };
+            }
+
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine(person[i].ToString());
+                
+            }
+
+            Console.ReadKey();
         }
     }
 }
